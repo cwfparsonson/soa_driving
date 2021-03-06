64458e8b71c4267ba9ccee1b987ad846d1ac09cf
billvamva <billvamva@hotmail.com>
2021-03-06 17:30:36 +0000

    ''

--- pso/soa/optimisation.py
+++ pso/soa/optimisation.py
@@ -1125,7 +1125,9 @@ class PSO:
                     x[n, g] = p[g]
                     
                 gbest_cost = fitness[i]
+                gbest_cost_history = np.delete([gbest_cost_history], [-1])
                 gbest_cost_history = np.append([gbest_cost_history], [gbest_cost])
+                iter_gbest_reached = np.delete([iter_gbest_reached], [-1])
                 iter_gbest_reached = np.append([iter_gbest_reached], [curr_iter])
                 cost_reduction = ((gbest_cost_history[0] - gbest_cost) \
                     / gbest_cost_history[0])*100
