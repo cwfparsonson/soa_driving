bc2f9240844c64afcd0254426e269c6183d636a8
billvamva <billvamva@hotmail.com>
2021-03-13 14:38:20 +0000

    ''

--- pso/soa/chaos_optimization.py
+++ pso/soa/chaos_optimization.py
@@ -75,13 +75,13 @@ class chaos:
             p = np.copy(dummy[np.argsort(dummy_value)[0]])
             
             # Random Cascaded SOAs
-            c = random.choice([0, 1, 2, 3, 4])
+            c = np.random.randint(0, 4)
             
             # Logistic Mapping/Tent Mapping
             z = self.mapping(z)
 
             # Randomize part of particle using chaotic mapping
-            for g in range(0, 2 *self.m):
+            for g in range(c * self.m, (c + 1) *self.m):
                 
                 p[g] = np.interp(z[g], [0, 1], [self.LB[g], self.UB[g]])
             
