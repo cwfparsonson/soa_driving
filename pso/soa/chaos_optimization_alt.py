a3cf2915bd00864c603bdaa7bfb02151a898de8b
billvamva <billvamva@hotmail.com>
2021-03-14 12:44:22 +0000

    ''

--- pso/soa/chaos_optimization_alt.py
+++ pso/soa/chaos_optimization_alt.py
@@ -80,7 +80,7 @@ class chaos:
             z = self.mapping(z)
 
             # Randomize part of particle using chaotic mapping
-            for g in range(0, 2 * self.m):
+            for g in range((c - 1) * self.m, c * self.m):
                 if random.uniform(0,1) > 0.3:
                     p[g] = np.interp(z[g], [0, 1], [self.LB[g], self.UB[g]])
             
