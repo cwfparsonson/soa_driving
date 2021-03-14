64843b17f94a449c266cf442fd7610a4e68e8377
billvamva <billvamva@hotmail.com>
2021-03-14 20:16:11 +0000

    ''

--- pso/soa/chaos_optimization.py
+++ pso/soa/chaos_optimization.py
@@ -81,7 +81,7 @@ class chaos:
 
             # Randomize part of particle using chaotic mapping
             for g in range(0, self.m):
-                p[g] = np.interp(z[g], [0, 1], [self.min_val, self.max_val])
+                p[g] = (np.interp(z[g], [0, 1], [self.min_val, self.max_val]) + p[g]) / 2.0
             
             # Get and Evaluate Output
             fitness[i] = self.get_cost(p)
