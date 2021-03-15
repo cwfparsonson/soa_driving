5fdd1b5b0892611aca1cc5faa7c5e6d54b098b0c
billvamva <billvamva@hotmail.com>
2021-03-15 11:22:15 +0000

    ''

--- project.py
+++ project.py
@@ -34,7 +34,7 @@ if __name__ == '__main__':
 
     # set PSO params
     n = 20
-    run = '2REAL_SPLIT_NO_CHANGE_40_REPS_GBEST_TENT'
+    run = 'NO_CHANGE_40_REPS_GBEST'
     iter_max = 100
     rep_max = 1 
     max_v_f = 0.05 
