f05b79deb8d7ef022fb08f1065a3904041642fa9
billvamva <billvamva@hotmail.com>
2021-03-15 14:16:40 +0000

    ''

--- pso/soa/chaos_optimization_alt.py
+++ pso/soa/chaos_optimization_alt.py
@@ -125,7 +125,7 @@ class chaos:
                     x[-1, g] = p[g]
                 
                 if self.change_range:
-                    self.a = self.a * (1- (gbest_cost_history[-1] - fitness[i]) / gbest_cost_history[-1])
+                    self.a = self.a * (1- 0.05)
                     for g in range(0, self.m):
                         self.LB[g] = max(self.LB[g], gbest[g] - self.a * (self.UB[g] - self.LB[g]))
                         self.UB[g] = min(self.UB[g], gbest[g] + self.a * (self.UB[g] - self.LB[g]))
