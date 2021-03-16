61c5215b4b05f7a214aae437751f0fd5a968574f
billvamva <billvamva@hotmail.com>
2021-03-16 17:03:56 +0000

    ''

--- project.py
+++ project.py
@@ -34,8 +34,8 @@ if __name__ == '__main__':
 
     # set PSO params
     n = 10
-    run = 'basev1'
-    iter_max = 20
+    run = 'basev1.1'
+    iter_max = 100
     rep_max = 1 
     max_v_f = 0.05 
     init_v_f = max_v_f 
